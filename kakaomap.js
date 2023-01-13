4f6c9e3a0e4c4b10ed47dd2dfcc4f7e056652499
BIX5_leh <kkh60245@gmail.com>
2023-01-13 17:29:26 +0900

    정리

--- kakaomap.js
+++ kakaomap.js
@@ -21,6 +21,7 @@ const csv_data = [];      // object[arr]
 let marker = [];        // object[arr] 생성된 maker들 모임
 let infoWindows = [];   // object[arr] 생성된 infoWindeow들 모임
 const ColumnData = {};    // obejct[object]
+const markerSize = new kakao.maps.Size(10, 10);
 const marker_green = "./greencircle.png";
 const marker_orange = "./orangecircle.png";
 const marker_red = "./redcircle.png";
@@ -82,9 +83,9 @@ function createIw() {
         let position = new kakao.maps.LatLng(parseFloat(csv_data[i].latlng[0]), parseFloat(csv_data[i].latlng[1]))
         let iwContent = `<div><p>거리 : ${csv_data[i].dist}</p><p>비고 : ${csv_data[i].note}</p></div>` // 인포 윈도우 내용 설정
         let infowindow = new kakao.maps.InfoWindow({
+            position: position,
             content: iwContent,
-            removable: true,
-            position: position
+            removable: true
         });
         infoWindows.push(infowindow);
     }
@@ -102,11 +103,33 @@ function deleteIw(iws) {
     }
 }
 
+
+/** 현제 선택된 radio의 설정에 맞는 slider를 설정하는 함수
+ * html selectbox에서 버튼을 누를때마다 호출
+ * input id[string]
+ */
+function setSlider(getId) {
+
+    currently_radio_type = getId;
+    $(setMarkerImage());
+    if (currently_radio_type === "radio-All") {
+        document.getElementById("select_range").style.visibility = 'hidden';
+    }
+    else if (["radio-SPI_1", "radio-SPI_2", "radio-SPI_3"].includes(currently_radio_type)) {
+        document.getElementById("select_range").style.visibility = 'visible';
+        makeSliderAndAmount(true, 0, 10, 2, [0, 10]);
+    }
+    else {
+        document.getElementById("select_range").style.visibility = 'visible';
+        makeSliderAndAmount(true, 0, 10, 1, [0, 10]);
+    }
+}
+
 /** 받아들인 정보로 slider와 amount를 생성하는 함수
  * input range[boolen], number, number, number, number[arr]
  */
 function makeSliderAndAmount(range, min, max, step, values) {
-
+    
     $("#slider-range").slider({
         range: range,
         min: min,
@@ -122,34 +145,11 @@ function makeSliderAndAmount(range, min, max, step, values) {
     $("#amount").val($("#slider-range").slider("values", 0) + " - " + $("#slider-range").slider("values", 1));
 }
 
-/** 현제 선택된 radio의 설정에 맞는 slider를 설정하는 함수
- * html selectbox에서 버튼을 누를때마다 호출
- * input id[string]
- */
-function setSlider(getId) {
-
-    currently_radio_type = getId;
-    if (currently_radio_type === "radio-All") {
-        document.getElementById("select_range").style.visibility = 'hidden';
-        setMarkers(currently_radio_type);
-    }
-    else if (["radio-SPI_1", "radio-SPI_2", "radio-SPI_3"].includes(currently_radio_type)) {
-        document.getElementById("select_range").style.visibility = 'visible';
-        setMarkers(currently_radio_type);
-        makeSliderAndAmount(true, 0, 10, 2, [0, 10]);
-    }
-    else {
-        document.getElementById("select_range").style.visibility = 'visible';
-        setMarkers(currently_radio_type);
-        makeSliderAndAmount(true, 0, 10, 1, [0, 10]);
-    }
-}
-
 /** slider에서 지정된 범위 scales[array]의 조건에 맞는 marker의 선명도(opacity)를 설정 
  * input slider.ui.values[arr]
 */
 function setMarkerOpacityByScale(scales) {
-
+    
     let cnt = 0;
     if (currently_radio_type !== 'radio-All') {
         for (let i of ColumnData[currently_radio_type.split('-')[1]]) {
@@ -161,30 +161,32 @@ function setMarkerOpacityByScale(scales) {
 
 /** Radio에서 선택되면 marker를 만들고 설정하는 함수. */
 function setMarkers() {
-
-    deleteIw(infoWindows)
-    deleteMarkers(marker)
-    marker = []
-
-    let markerSize = new kakao.maps.Size(10, 10);
+    
     for (let i = 0; i < csv_data_length; i++) { // 마커 하나씩 지정해서 대입
         let position = new kakao.maps.LatLng(parseFloat(csv_data[i].latlng[0]), parseFloat(csv_data[i].latlng[1]))
-
-        markercolor = (currently_radio_type === 'radio-All') ? marker_blue : getColor(parseFloat(csv_data[i][currently_radio_type.split('-')[1]]))
-        let markerImage = new kakao.maps.MarkerImage(markercolor, markerSize)
+        
         marker[i] = new kakao.maps.Marker({
             map: map,
             position: position,
-            image: markerImage,
             clickable: true
         });
+
         // 클릭 리스너, 다른 차트와 연동할 때 사용
         kakao.maps.event.addListener(marker[i], 'click', function () {
             selectData(i)
-        }
-        );
+        });
     }
+    setMarkerImage();
+}
 
+function setMarkerImage(){
+    
+    for (let i = 0; i < csv_data_length; i++) { // 마커 하나씩 지정해서 대입
+        markercolor = (currently_radio_type === 'radio-All') ? 
+            marker_blue : getColor(parseFloat(csv_data[i][currently_radio_type.split('-')[1]]));
+        let markerImage = new kakao.maps.MarkerImage(markercolor, markerSize);
+        marker[i].setImage(markerImage);
+    }
 }
 
 /** marker[arr]에 저장된 모든 값을 지우는 함수
@@ -206,6 +208,7 @@ $(function () {
         url: fileName,
         dataType: "text",
         success: function (data) {
+
             let allRow = data.split("\n");
             for (let i = 14; i < allRow.length - 1; i++) {
                 let column = allRow[i].split(",")
@@ -217,17 +220,14 @@ $(function () {
                 })
             }
             csv_data_length = csv_data.length;
+
             for (let key of Object.keys(csv_data[0])) {
                 ColumnData[key] = [];
                 for (let i = 0; i < csv_data_length; i++) {
                     ColumnData[key].push(csv_data[i][key])
                 }
             }
-            let position = new kakao.maps.LatLng(parseFloat(csv_data[0].latlng[0]), parseFloat(csv_data[0].latlng[1]))
-            // 여기에 함수 추가.
-            map.setCenter(position)
-            createIw();
-            setMarkers();
+            
             valueInitialize();     
         }
     });
@@ -235,6 +235,10 @@ $(function () {
 
 /** 초기값 설정 함수 */
 function valueInitialize() {
+    let position = new kakao.maps.LatLng(parseFloat(csv_data[0].latlng[0]), parseFloat(csv_data[0].latlng[1]))
+    map.setCenter(position);
+    createIw();
+    setMarkers();
     setText(parseFloat(ColumnData.dist[csv_data_length - 1]).toFixed(3) + " km", "dist");
     for (let key of ["pd", "roughness", "amount_crack", "ratio_crack", "SPI_1", "SPI_2", "SPI_3"]) {
         setText(getAvg(ColumnData[key]).toFixed(3), key)
@@ -279,6 +283,58 @@ function makeChart() {
             }
         }
         myChart[key] = new Chart(key + '_Chart', makeChartData(sum));
+    }
+}
+
+/** Chart 생성자에 들아갈 data를 만들어 주는 함수
+ * input L,M,H_data[arr]
+ * return chart_data[object]
+ */
+function makeChartData(dataArr) {
+
+    // key로 label 값 정해주기
+    return {
+        type: 'bar',
+        data: {
+            labels: ['L', 'M', 'H'],
+            datasets: [{
+                data: dataArr,
+                datalabels: {
+                    color: 'black',
+                    font: { size: 12 },
+                    offset: 3,
+                    anchor: 'end',
+                    clamp: true,
+                    clip: false,
+                    align: 'top'
+                },
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+            }]
+        },
+        plugins: [ChartDataLabels],
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: { // y축 줄당 표시 값
+                        stepSize: 2
+                    }
+                },
+                x: {
+                    beginAtZero: true,
+                    type: 'category',
+                }
+            }
+        }
     }
 }
 
@@ -376,58 +432,6 @@ function selectData(selectedRow) {
     }
     map.setCenter(position) // 선택한 마커 중심으로 맵 이동
     selected = index
-}
-
-/** Chart 생성자에 들아갈 data를 만들어 주는 함수
- * input L,M,H_data[arr]
- * return chart_data[object]
- */
-function makeChartData(dataArr) {
-
-    // key로 label 값 정해주기
-    return {
-        type: 'bar',
-        data: {
-            labels: ['L', 'M', 'H'],
-            datasets: [{
-                data: dataArr,
-                datalabels: {
-                    color: 'black',
-                    font: { size: 12 },
-                    offset: 3,
-                    anchor: 'end',
-                    clamp: true,
-                    clip: false,
-                    align: 'top'
-                },
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                ],
-            }]
-        },
-        plugins: [ChartDataLabels],
-        options: {
-            plugins: {
-                legend: {
-                    display: false
-                }
-            },
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    ticks: { // y축 줄당 표시 값
-                        stepSize: 2
-                    }
-                },
-                x: {
-                    beginAtZero: true,
-                    type: 'category',
-                }
-            }
-        }
-    }
 }
 
 /** 만들어진 chart 안의 데이터 하나를 지우는 함수
